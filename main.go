a5fac629734460914f4c973813f76c87dfbc2bf6
tharindu.epasingha <tharindu.epasingha@itechro.com>
2024-12-10 15:00:27 +0530

    configured order routes and route handlers

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"tharinduEpaz/go-microservice/application"
 )
 
 func main() {
-	// has New then it is the constructer
-	// best practise to use the constructor than directly instanciating the instance
-	router := chi.NewRouter()
+	app := application.New()
+	
+	err := app.Start(context.TODO())
 
-	//logger is used to log the request and chi provides one out of the box
-	router.Use(middleware.Logger)
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("Failed to start the server", err)
+		fmt.Println("failed to start app : ", err)
 	}
-}
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
 }
