54d40a7586f9147d8081bb011f7925e8b261824f
Tharindu Epasingha <epazingha@gmail.com>
2024-12-16 12:17:04 +0530

    feat: added graceful shutdown with context and signal handling

--- application/app.go
+++ application/app.go
@@ -4,17 +4,22 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 // by defining the router inside the App struct make this decoupled from chi
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 	return app
 }
@@ -25,9 +30,44 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
+	// connet to the database
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server : %w", err)
+		return fmt.Errorf("faied to connet to redis: %w", err)
 	}
-	return nil
+
+	// close the database at the end of the start function
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("Starting server")
+
+	ch := make(chan error, 1) // second arguement is the buffer size
+
+	// run the server in a separate go routine
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server : %w", err)
+		}
+		close(ch)
+	}()
+
+	// This waits for either:
+	// An error from the server (through the channel)
+	// A signal to shut down (through the context)
+	// If shutdown is requested,
+	// it gives the server 10 seconds to finish any ongoing requests before forcing a shutdown.
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		fmt.Println("Shutting down the server")
+		return server.Shutdown(timeout)
+	}
 }
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"tharinduEpaz/go-microservice/application"
 )
 
 func main() {
 	app := application.New()
-	
-	err := app.Start(context.TODO())
 
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel() // call at the end of the current function
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app : ", err)
 	}
